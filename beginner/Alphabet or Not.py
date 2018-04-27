9ecae47ceb2e6259820b6f2aba955dd071559452
aslam7825 <36443478+aslam7825@users.noreply.github.com>
2018-04-27 11:22:27 +0530

    Create Alphabet or Not.py

---
+++ beginner/Alphabet or Not.py
@@ -0,0 +1,7 @@
+list=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+x=raw_input("")
+if (x in list):
+    print (" Alphabet")
+
+else:
+    print ("No")
